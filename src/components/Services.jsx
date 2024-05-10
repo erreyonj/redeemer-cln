e3cf739ed4434e45945e3ed11dcf214887185ef6
erreyonj <erreyon.js@gmail.com>
2024-05-10 02:24:38 -0500

    further polishing of responsiveness

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -18,24 +18,26 @@ const Services = (isPage) => {
 
     // Component Styling
     // optional tailwind classes if this component is embed level or landing page level
-    const pageClass = 'h-[450px] grid place-content-space-between lg:grid-cols-50-50'
-    const notPageClass = 'h-[450px] flex flex-row place-content-center' 
-
-    // Component Functionality
+    const pageClass = 'h-[450px] mt-20 flex flex-row place-content-center lg:grid-cols-50-50 lg:mx-10'
+    const notPageClass = 'h-[450px] flex flex-row place-content-center lg:mx-10' 
     // returns a tailwind class for ordering elements on a page
     const second = () => 'order-2'
     const first = () => 'order-1'
 
+
+    // Component Functionality
+    
+
     console.log(currentPage);
 
   return (
-    <section id="sectionServices" className={currentPage==='Services'?pageClass:notPageClass}>
+    <section id="sectionServices" className={currentPage==='/Services'?pageClass:notPageClass}>
         <div id="serviceDescription" className={`flex flex-col place-content-center my-5 ${(currentPage.slice(0,1)==='/')&&second()}`}>
-            <h2 className='text-4xl text-black font-bebas mx-auto'>Our Services</h2>
+            <h2 className='text-4xl text-black text-center font-bebas mx-3'>Our Services</h2>
             <p className="hidden md:block lg:w-1/2 md:mx-auto text-center">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas dignissimos, aliquid, recusandae pariatur obcaecati voluptate soluta aut eos dicta cupiditate eum ratione illo id. Officiis sequi eius cupiditate repudiandae quaerat.</p>
         </div>
         <div id="swipeContainer" className={`p-3 h-96 md:h-[520px] lg:w-[1830px] ${(currentPage==='Services')&&first()} overflow-hidden`}>
-            <div id="serviceCards" className={`h-[350px] md:w-[80] lg:w-[675px] md:h-[400px] py-5 ${isPage?'':''} gap-5 flex flex-row place-content-between lg:flex-nowrap overflow-hidden snap-x *:mx-5  md:h-auto `}> {/* <-- maybe add shadow around cards here? shadow-slate-300 shadow-inner*/}
+            <div id="serviceCards" className={`h-[350px] md:w-[80] lg:w-[675px] md:h-[400px] py-5 ${isPage?'':''} gap-5 flex flex-row place-content-between overflow-scroll lg:flex-nowrap snap-x *:mx-5  md:h-auto `}> {/* <-- maybe add shadow around cards here? shadow-slate-300 shadow-inner*/}
                 {ourServices.map((service,i)=><ServiceCard key={i} title={service} description={serviceDescriptions[i]}/>)}
             </div>
             <div id="teamMemberScreen" className={`hidden xl:block absolute top-[495px] left-[615px] h-[415px] w-28 bg-gradient-to-l to-transparent from-white`}> </div>
