7da7b75c5cec9600e6741fe8bcf5eb022f0ba83e
erreyonj <erreyon.js@gmail.com>
2024-05-14 12:34:11 -0500

    further deploy testing

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/redeemer-cln/',
+  base:"/redeemer-cln/",
+  build: {
+    outDir: "build"
+  },
   server: {
     port: 3000,
   },
